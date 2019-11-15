0b2ed6d414621014687c0d5ab6b0f83233b152ff
ELCHE\adolfojoa <adolfojoa@ext.tempe.es>
2019-11-15 08:53:12 +0100

    udpate exercise

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,19 +1,32 @@
 import React, { useState } from 'react';
 import moment from 'moment';
-import DateFnsUtils from '@date-io/date-fns';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
 import Typography from '@material-ui/core/Typography';
 import DialogActions from '@material-ui/core/DialogActions';
 import Button from '@material-ui/core/Button';
-import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200,
+  },
+}));
 const Modal = ({ handleClose, open, libro, alquilar }) => {
   const [selectedDate, setSelectedDate] = useState(new Date());
+  const classes = useStyles();
 
-  const handleDateChange = (date) => {
-    setSelectedDate(date);
+  const handleDateChange = (e) => {
+    console.log('date', e.target.value);
+    setSelectedDate(e.target.value);
   };
 
   return (
@@ -27,19 +40,23 @@ const Modal = ({ handleClose, open, libro, alquilar }) => {
             .add('15', 'days')
             .format('DD [de] MMMM')}
         </Typography>
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-          <KeyboardDatePicker
-            margin='normal'
-            id='date-picker-dialog'
-            label='Date picker dialog'
-            format='MM/dd/yyyy'
-            value={selectedDate}
-            onChange={handleDateChange}
-            KeyboardButtonProps={{
-              'aria-label': 'change date',
-            }}
-          />
-        </MuiPickersUtilsProvider>
+        <TextField
+          id='date'
+          label='Fecha de pedido'
+          type='date'
+          onChange={handleDateChange}
+          value={selectedDate}
+          className={classes.textField}
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+        <Typography variant='body2'>
+          Debes devolverlo antes del
+          {moment(selectedDate)
+            .add('15', 'days')
+            .format('DD [de] MMMM')}
+        </Typography>
       </DialogContent>
       <DialogActions>
         <Button color='primary' variant='contained' onClick={() => alquilar(libro)}>
